98e629cd95a060ebb06472f19bbc750b9527c877
Orlando <orlandomonri@gmail.com>
2025-07-22 21:20:19 -0600

    Se implementaron iconos sobre el plato para saber que contiene. (PlateIconsUI)

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -42,6 +42,11 @@ public class PlateKitchenObject : KitchenObject
             });
             return true;
         }
-        
     }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
+    }
+
 }
